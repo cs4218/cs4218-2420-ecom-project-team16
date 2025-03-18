0a0e22ef4efbd6e06fd7d2c7a349fa928ba5887d
zzthian <zzthian@hotmail.com>
2025-03-18 16:27:01 +0800

    Add UI test for user Dashboard

---
+++ playwright-tests/DashboardUI.spec.ts
@@ -0,0 +1,37 @@
+import { test, expect } from "@playwright/test";
+import JWT from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const fakeUser = {
+  _id: "faker",
+  name: "Faker",
+  email: "faker@example.com",
+  address: "fake address",
+};
+const fakeStorageItem = {
+  success: true,
+  user: fakeUser,
+  token: JWT.sign({ _id: fakeUser._id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  }),
+};
+
+console.log(process.env.JWT_SECRET);
+test("Dashboard displays user information correctly", async ({ page }) => {
+  await page.addInitScript((data) => {
+    localStorage.setItem("auth", JSON.stringify(data));
+  }, fakeStorageItem);
+
+  await page.goto("http://localhost:3000/dashboard/user");
+
+  await expect(page.locator("h3")).toHaveText([
+    fakeUser.name,
+    fakeUser.email,
+    fakeUser.address,
+  ]);
+  await expect(page.getByRole("heading", { name: "Dashboard" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "Profile" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "Orders" })).toBeVisible();
+});
