7b4618c68052fb813d3d6504da04f244fe5b7503
Sean <seanpzk2@gmail.com>
2025-03-23 18:49:58 +0800

    Attempt to fix UI test

--- old-playwright.config.cjs
+++ old-playwright.config.cjs
@@ -30,6 +30,9 @@ module.exports = defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+
+    storageState: 'playwright-tests/context/adminAuth.json',
+    headless: true
   },
 
   /* Configure projects for major browsers */
