8bfb110a8e90cf4d8fa53e5363f8f3a87ad73920
Izz Hafeez <izzhafeez@gmail.com>
2025-03-23 18:32:08 +0800

    Add Header test

--- client/src/components/HeaderIntegration.test.js
+++ client/src/components/HeaderIntegration.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import "@testing-library/jest-dom/extend-expect";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
-import CartPage from "./CartPage";
+import Header from "./Header";
 import { CartProvider } from "../context/cart";
 import { AuthProvider } from "../context/auth";
 import { SearchProvider } from "../context/search";
@@ -33,29 +33,7 @@ const mockCart = [
 ]
 
 describe("Cart Page Integration Test", () => {
-    test("Should render admin dashboard with related components", async() => {
-        localStorage.setItem('cart', JSON.stringify(mockCart));
-
-        render(
-            <AuthProvider value={[mockAuth]}>
-                <CartProvider>
-                    <SearchProvider>
-                        <MemoryRouter>
-                            <CartPage />
-                        </MemoryRouter>
-                    </SearchProvider>
-                </CartProvider>
-            </AuthProvider>
-        );
-        
-        // cart
-        expect(screen.getByText("Cart")).toBeInTheDocument();
-        expect(screen.getByText("The Law of Contract in Singapore")).toBeInTheDocument();
-        expect(screen.getByText("NUS T-shirt")).toBeInTheDocument();
-        expect(screen.getByText("Total: $59.98")).toBeInTheDocument();
-    })
-
-    test("Should render correct user details", () => {
+    test("Should render correct name", async() => {
         localStorage.setItem('auth', JSON.stringify(mockAuth));
 
         render(
@@ -63,18 +41,18 @@ describe("Cart Page Integration Test", () => {
                 <CartProvider>
                     <SearchProvider>
                         <MemoryRouter>
-                            <CartPage />
+                            <Header/>
                         </MemoryRouter>
                     </SearchProvider>
                 </CartProvider>
             </AuthProvider>
         );
-
+        
+        // cart
         expect(screen.getByText("Hong Shan")).toBeInTheDocument();
-        expect(screen.getByText("Redhill")).toBeInTheDocument();
     });
 
-    test("Should remove item from cart", () => {
+    test("Should render correct number of cart items", async() => {
         localStorage.setItem('cart', JSON.stringify(mockCart));
 
         render(
@@ -82,20 +60,14 @@ describe("Cart Page Integration Test", () => {
                 <CartProvider>
                     <SearchProvider>
                         <MemoryRouter>
-                            <CartPage />
+                            <Header/>
                         </MemoryRouter>
                     </SearchProvider>
                 </CartProvider>
             </AuthProvider>
         );
-
-        const removeButton = screen.getAllByText("Remove")[0];
-        expect(removeButton).toBeInTheDocument();
-
-        removeButton.click();
-
-        // check local storage
-        const cart = JSON.parse(localStorage.getItem('cart'));
-        expect(cart).toEqual([mockCart[1]]);
+        
+        // cart count is in sup
+        expect(screen.getByText("2")).toBeInTheDocument();
     });
 });
